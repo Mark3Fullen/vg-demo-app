389a5b69b929e7d7fd9f9d1a6a8835c97cd648ba
Mark <mark3fullen@gmail.com>
2023-01-09 20:26:47 -0500

    deleted Navlink

--- src/Components/Tiktaktoe.js
+++ src/Components/Tiktaktoe.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import '../Styles/tiktaktoe.css';
 import './tiktaktoe-app-comp/tiktoktoe-app-styles/score.css';
 
