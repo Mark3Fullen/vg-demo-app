d353ac672a7a9c55212fdb27d55313194d2081b9
Mark <mark3fullen@gmail.com>
2023-01-16 20:11:54 -0500

    completed functionality/added classnames dependancy

--- src/Components/memory-card-app-comp/Memorycard.js
+++ src/Components/memory-card-app-comp/Memorycard.js
@@ -1,21 +1,23 @@
 import React from "react";
 
+import classnames from 'classnames';
+
 import './memorycardcard.css';
 
 import pokecard from './imgs/ballcard.jpg';
 
-const Memorycard = ({ onClick, card, index, isFlipped, isDisabled }) => {
+const Memorycard = ({ onClick, card, index, isFlipped, isDisabled, isInactive }) => {
     
     const handleClick = () => {
         !isFlipped && !isDisabled && onClick(index);
     };
 
     return (
-        <div className="card" onClick={handleClick}>
+        <div className={classnames("card", {"is-flipped": isFlipped, "is-inactive": isInactive})} onClick={handleClick}>
             <div className="card-face">
                 <img src={pokecard} alt="pokecard" style={{ height: 100, width: 100 }} />
             </div>
-            <div className="card-face">
+            <div className="card-face card-back-face">
                 <img src={card.image} alt="pokeball" style={{ height: 100, width: 100 }}/>
             </div>
         </div>
