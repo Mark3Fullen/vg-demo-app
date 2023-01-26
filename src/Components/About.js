8434c08da995119c2612158aa57627cefe873984
Mark <mark3fullen@gmail.com>
2023-01-26 18:46:54 -0500

    added about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,8 +14,14 @@ const About = () => {
 
             </div>
 
-            <div>
-                Bruh what
+            <div className="aboutBody">
+                <h4>This is a simple React flash game app to display flash games I found around the web built in React. Credit to the creators of the original apps are mentioned here.</h4>
+                <div className="aboutBodyLinks">
+                <p><span>TikTakToe --- </span><a href="https://github.com/NedimBegic/React-Tik-Tak-Toe?ref=reactjsexample.com">Github Repository</a></p>
+                <p><span>Memory Card game --- </span><a href="https://github.com/lorenzo774/memory-card?ref=reactjsexample.com">Github Repository</a></p>
+                <p><span>Lights Out --- </span><a href="https://github.com/luciatruden/lights-out?ref=reactjsexample.com">Github Repository</a></p>
+                <p><span>Tenzies --- </span><a href="https://github.com/sachinkumar1469/Tenzies_game_app_using_react?ref=reactjsexample.com">Github Repository</a></p>
+                </div>
             </div>
         
         </div>
